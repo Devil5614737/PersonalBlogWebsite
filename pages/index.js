c364e7a1660fd715a87499f0b136e5b353a17eef
kaushik <kaushiksheel9@gmail.com>
2022-02-14 21:52:07 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router'
 import Navbar from '../components/navbar'
 import styles from '../styles/Home.module.css'
+
 
 
 export default function Home() {
@@ -16,6 +18,36 @@ router.push('/blog')
 
   return (
     <>
+   <Head>
+    
+    <meta
+      name="description"
+      content="Free code tutorials ,website clones, Coding Blogs,Coding Solutions"
+    />
+    <meta
+      name="keywords"
+      content="HTML, CSS, JavaScript Sass React Styled-components Firebase Clone Demo Github Coding Blogs"
+    />
+
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:image"
+      content="blob:https://personal-blog-website-psi.vercel.app/blog#"
+    />
+    <meta name="author" content="Kaushik sheel" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Coding Hub</title>
+    <meta property="og:title" content="Coding Blogs and tutorials" />
+    <meta
+      property="og:description"
+      content="Free code tutorials ,coding blogs"
+    />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"/>
+
+  </Head>
   <Navbar/>
   <main className={styles.main}>
     <div className={styles.mainContainer}>
